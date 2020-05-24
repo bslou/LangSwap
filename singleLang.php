881bf5449805aa492ef21617fc5849f5c1e10c77
bslou <64224906+bslou@users.noreply.github.com>
2020-05-23 21:06:18 -0700

    Create singleLang.php

---
+++ singleLang.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>LangSwap - SinglePlayer</title>
+    <link rel = "icon" href = "https://freesvg.org/img/internet-web-browser.png">
+  <style>
+
+
+
+  </style>
+  </head>
+  <body>
+
+  </body>
+
+</html>
