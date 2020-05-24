f54cefd12ae36af4b37a88781a9377672921e2ac
bslou <64224906+bslou@users.noreply.github.com>
2020-05-23 21:02:38 -0700

    Create interact.php

---
+++ interact.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Interaction info - LangSwap</title>
+
+
+<style>
+.box{
+  display: inline;  
+  overflow: auto;
+  width: 60%;
+  height: 60%;
+  border: 1px solid black;
+  font-size: 16pt;
+}
+
+
+</style>
+
+</head>
+
+<body>
+
+<center><h1>Interaction with Others information</h1></center>
+
+<br><br><br>
+
+
+<center><p class = "box">
+
+Through this website, you will have the ability to interact with others through video call and messaging. There will be a calendar button to plan out times for when you want to meet up with someone else. People can leave chats if they want too, they just have to contact the other person. 
+
+</p></center>
+
+</body>
+
+</html>
